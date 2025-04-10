3bc37fedb58ba0b8189d5ffacbded698f6784c4a
billjordanchess <53726859+billjordanchess@users.noreply.github.com>
2025-04-10 13:52:18 +0930

    Update see.cpp

--- see.cpp
+++ see.cpp
@@ -1,10 +1,10 @@
 #include "globals.h"
 
-int ThreatScore(const int s, const int xs, const int sq, const int p, const int best);
+int AttackKing(const int s, const int sq);
 
 int p_value[6] = { 1,3,3,5,9,0 };
 
-int SEESearch(int s, int attacker, const int sq)
+int SEE(int s, int attacker, const int sq)
 {
 	int piece;
 	int count = 0;
@@ -66,13 +66,11 @@ int SEESearch(int s, int attacker, const int sq)
 	return total_score;
 }
 
-int BestThreat(const int s, const int xs, const int diff)
+bool BestThreat(const int s, const int xs, const int diff)
 {
-	if (diff >= 9)
-		return 0;
+	if (diff >= QVAL)
+		return false;
 	int sq, i, x;
-
-	int best = 0;
 
 	BITBOARD b1, b2;
 
@@ -84,87 +82,104 @@ int BestThreat(const int s, const int xs, const int diff)
 		b1 &= not_mask[i];
 		sq = pawnplus[s][i];
 		if (b[sq] == EMPTY && Attack(xs, sq) == 0 &&
-			!(mask_cols[i] & mask[pieces[xs][R][0]] && !(bit_between[i][NextBit(pieces[xs][R][0])] & bit_all)))
+			!(mask_cols[i] & mask[pieces[xs][R][0]] && 
+			!(bit_between[i][NextBit(pieces[xs][R][0])] & bit_all)))
 		{
-			return i;
+			return true;
 		}
 	}
 
-	BITBOARD bu = bit_pieces[xs][Q];
+	BITBOARD bit_targets = bit_pieces[xs][Q];
 
-	if (diff < 5)
+	if (diff < RVAL)//Rooks are potentially a target.
 	{
-		bu |= bit_pieces[xs][R];
-		if (diff < 3)
+		bit_targets |= bit_pieces[xs][R];
+		if (diff < BVAL)//Minor pieces are potentially a target.
 		{
-			bu |= bit_pieces[xs][N] | bit_pieces[xs][B];
-			if (diff < 1)
-				bu |= bit_pieces[xs][P];
+			bit_targets |= bit_pieces[xs][N] | bit_pieces[xs][B];
+			if (diff < PVAL)//Pawns are potentially a target.
+				bit_targets |= bit_pieces[xs][P];
 		}
 	}
-	if (bu == 0)
-		return 0;
+	if (bit_targets == 0)
+		return false;
 
-	if (s == 0)
+	if (s == 0)//Are there any pawn attacks?
 	{
-		b1 = bu & (((bit_pieces[0][P] & not_h_file) << 9));
-		b2 = bu & (((bit_pieces[0][P] & not_a_file) << 7));
+		b1 = bit_targets & (((bit_pieces[0][P] & not_h_file) << 9));
+		b2 = bit_targets & (((bit_pieces[0][P] & not_a_file) << 7));
 	}
 	else
 	{
-		b1 = bu & (((bit_pieces[1][P] & not_h_file) >> 7));
-		b2 = bu & (((bit_pieces[1][P] & not_a_file) >> 9));
+		b1 = bit_targets & (((bit_pieces[1][P] & not_h_file) >> 7));
+		b2 = bit_targets & (((bit_pieces[1][P] & not_a_file) >> 9));
 	}
 
 	while (b1)
 	{
 		sq = NextBit(b1);
 		b1 &= not_mask[sq];
-		if (bit_left[!s][sq] & bit_pieces[s][P] & bit_all)
+		if (b[sq] > P || Attack(xs, sq) == 0)
 		{
-			i = pawnleft[!s][sq];
+			return true;
 		}
-		if (ThreatScore(s, xs, sq, i, best))
-			return 1;
+		if (SEE(s, pawnleft[!s][sq], sq) > 0)
+		{
+			return true;
+		}
 	}
 
 	while (b2)
 	{
 		sq = NextBit(b2);
 		b2 &= not_mask[sq];
-		if (bit_right[!s][sq] & bit_pieces[s][P] & bit_all)
+		if (b[sq] > P || Attack(xs, sq) == 0)
 		{
-			i = pawnright[!s][sq];
+			return true;
 		}
-		if (ThreatScore(s, xs, sq, i, best))
-			return 1;
+		if (SEE(s, pawnright[!s][sq], sq) > 0)
+		{
+			return true;
+		}
 	}
 
 	for (x = 0; x < total[s][N]; x++)
 	{
 		i = pieces[s][N][x];
-		b1 = bit_knightmoves[i] & bu;
+		b1 = bit_knightmoves[i] & bit_targets;
 		while (b1)
 		{
 			sq = NextBit(b1);
 			b1 &= not_mask[sq];
-			if (ThreatScore(s, xs, sq, i, best))
-				return 1;
+			if (b[sq] > B || Attack(xs, sq) == 0)
+			{
+				return true;
+			}
+			else if (SEE(s, i, sq) > 0)
+			{
+				return true;
+			}
 		}
 	}
 
 	for (x = 0; x < total[s][B]; x++)
 	{
 		i = pieces[s][B][x];
-		b1 = bit_bishopmoves[i] & bu;
-		while (b1)
+		b1 = bit_bishopmoves[i] & bit_targets;
+		while (b1)//Are there any bishop attacks?
 		{
 			sq = NextBit(b1);
 			b1 &= not_mask[sq];
 			if (!(bit_between[i][sq] & bit_all))
 			{
-				if (ThreatScore(s, xs, sq, i, best))
-					return 1;
+				if (b[sq] > B || Attack(xs, sq) == 0)
+				{
+					return true;
+				}
+				else if (SEE(s, i, sq) > 0)
+				{
+					return true;
+				}
 			}
 		}
 	}
@@ -172,15 +187,21 @@ int BestThreat(const int s, const int xs, const int diff)
 	for (x = 0; x < total[s][R]; x++)
 	{
 		i = pieces[s][R][x];
-		b1 = bit_rookmoves[i] & bu;
-		while (b1)
+		b1 = bit_rookmoves[i] & bit_targets;
+		while (b1)//Are there any rook attacks?
 		{
 			sq = NextBit(b1);
 			b1 &= not_mask[sq];
 			if (!(bit_between[i][sq] & bit_all))
 			{
-				if (ThreatScore(s, xs, sq, i, best))
-					return 1;
+				if (b[sq] > R || Attack(xs, sq) == 0)
+				{
+					return true;
+				}
+				else if (SEE(s, i, sq) > 0)
+				{
+					return true;
+				}
 			}
 		}
 	}
@@ -188,8 +209,8 @@ int BestThreat(const int s, const int xs, const int diff)
 	for (x = 0; x < total[s][Q]; x++)
 	{
 		i = pieces[s][Q][x];
-		b1 = bit_queenmoves[i] & bu;
-		while (b1)
+		b1 = bit_queenmoves[i] & bit_targets;
+		while (b1)//Are there any queen attacks?
 		{
 			sq = NextBit(b1);
 			b1 &= not_mask[sq];
@@ -197,45 +218,84 @@ int BestThreat(const int s, const int xs, const int diff)
 			{
 				if (Attack(xs, sq) == 0)
 				{
-					return 1;
-				}
-				else
-				{
-					if (SEESearch(s, i, sq) > best)
-					{
-						return 1;
-					}
+					return true;
 				}
 			}
 		}
 	}
 
 	i = pieces[s][K][0];
-	b1 = bit_kingmoves[i] & bu;
-	while (b1)
+	b1 = bit_kingmoves[i] & bit_targets;
+	while (b1)//Are there any king attacks?
 	{
 		sq = NextBit(b1);
 		b1 &= not_mask[sq];
 		if (Attack(xs, sq) == 0)
 		{
-			return 1;
+			return true;
 		}
 	}
-	return 0;
+	return false;
 }
 
-int ThreatScore(const int s, const int xs, const int sq, const int a, const int best)
+int GetNextAttacker(const int s, const int sq)
 {
-	if ((p_value[b[sq]] > p_value[b[a]]) || Attack(xs, sq) == 0)
+	if (bit_pawndefends[s][sq] & bit_pieces[s][P] & bit_all)
 	{
-		return 1;
-	}
-	else
-	{
-		if (SEESearch(s, a, sq) > best)
+		if (bit_left[!s][sq] & bit_pieces[s][P] & bit_all)
 		{
-			return 1;
+			return pawnleft[!s][sq];
+		}
+		else
+		{
+			return pawnright[!s][sq];
 		}
 	}
-	return 0;
+
+	int i;
+
+	for (int x = 0; x < total[s][N]; x++)
+	{
+		i = pieces[s][N][x];
+		if (bit_knightmoves[sq] & mask[i] & bit_all)
+		{
+			return i;
+		}
+	}
+	for (int x = 0; x < total[s][B]; x++)
+	{
+		i = pieces[s][B][x];
+		if (bit_bishopmoves[sq] & mask[i] & bit_all)
+		{
+			if (!(bit_between[i][sq] & bit_all))
+			{
+				return i;
+			}
+		}
+	}
+	for (int x = 0; x < total[s][R]; x++)
+	{
+		i = pieces[s][R][x];
+		if (bit_rookmoves[sq] & mask[i] & bit_all)
+		{
+			if (!(bit_between[i][sq] & bit_all))
+			{
+				return i;
+			}
+		}
+	}
+	for (int x = 0; x < total[s][Q]; x++)
+	{
+		i = pieces[s][Q][x];
+		if (bit_queenmoves[sq] & mask[i] & bit_all)
+		{
+			if (!(bit_between[i][sq] & bit_all))
+			{
+				return i;
+			}
+		}
+	}
+	if (bit_kingmoves[sq] & bit_pieces[s][K] & bit_all)
+			return pieces[s][K][0];
+	return -1;
 }
